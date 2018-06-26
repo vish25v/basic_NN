a4986b328e0af579fb41cfa1e512c3944022adf1
vish2v <vaishakhi.verma25@gmail.com>
2018-06-26 14:03:32 -0400

    inital commit

---
+++ basic_NN.py
@@ -0,0 +1,16 @@
+import numpy as numpy
+
+#Sigmoid function:
+def nonlin(x,deriv-False):
+	if(deriv==True):
+		return x+(1_x)
+
+	return z/(1+np,exp(-x))
+
+#input data
+x=np.array([[0,0,1],
+	[0,1,1],
+	[1,0,1],
+	[1,1,1]])
+	
+#ou
